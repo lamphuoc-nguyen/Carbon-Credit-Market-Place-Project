6ec7fee1c0d15f105643ad828cbffcd49ef68d51
nguyen-hoangdai <nguyenhoangdai10042005@gmail.com>
2025-10-08 20:05:44 +0700

    login page

---
+++ frontend/login/src/components/SocialLogin.jsx
@@ -0,0 +1,16 @@
+const SocialLogin = () => {
+    return (
+        <div className="flex gap-5 mb-6">
+            <button className="flex gap-3 w-full text-base font-medium cursor-pointer py-3 rounded-md items-center justify-center bg-violet-50 border border-violet-300 hover:border-violet-600 hover:bg-violet-100 transition-all duration-300">
+                <img src="google.svg" alt="Google" className="w-6" />
+                Google
+            </button>
+            <button className="flex gap-3 w-full text-base font-medium cursor-pointer py-3 rounded-md items-center justify-center bg-violet-50 border border-violet-300 hover:border-violet-600 hover:bg-violet-100 transition-all duration-300">
+                <img src="apple.svg" alt="Apple" className="w-6" />
+                Apple
+            </button>
+        </div>
+    )
+}
+
+export default SocialLogin;
